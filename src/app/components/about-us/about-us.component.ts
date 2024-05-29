564137bca0d1b0b41119f84682288cfc021ed565
Farha Ahmed <farhaa853@gmail.com>
2024-05-30 00:17:20 +0300

    About-us page first version

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GalleriaModule } from 'primeng/galleria';
+import { FormComponent } from '../../shared/form/form.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [GalleriaModule, CommonModule, FormComponent],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.scss'
+  styleUrl: './about-us.component.scss',
 })
 export class AboutUsComponent {
+  images: string[] = ['../../../assets/About-us/galleria 1.jpg'];
 
+  displayCustom: boolean = false;
+
+  activeIndex: number = 0;
+
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1500px',
+      numVisible: 5,
+    },
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+    },
+  ];
+
+  imageClick(index: number) {
+    // this.activeIndex = index;
+    this.displayCustom = true;
+  }
 }
