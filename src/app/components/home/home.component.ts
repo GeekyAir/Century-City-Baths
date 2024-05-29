16750683fed8ce80fcbd92b1ddab58afd90ebec4
rania-ibrhem30 <raniamansour967@gmail.com>
2024-05-29 23:31:12 +0300

    home page update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormComponent } from '../form/form.component';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, CardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
