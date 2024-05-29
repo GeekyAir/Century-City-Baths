4ba682b4484d7760e4b97833930fbe054a70aece
rania-ibrhem30 <raniamansour967@gmail.com>
2024-05-29 16:17:18 +0300

    home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
