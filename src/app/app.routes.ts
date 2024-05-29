5bcf82558972f3bd8dd2671835c14819a92909c9
Farha Ahmed <farhaa853@gmail.com>
2024-05-29 22:08:39 +0300

    floder structuer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '**', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
-    { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
-
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'about-us',
+    loadComponent: () =>
+      import('./components/about-us/about-us.component').then(
+        (c) => c.AboutUsComponent
+      ),
+  },
 ];
-
