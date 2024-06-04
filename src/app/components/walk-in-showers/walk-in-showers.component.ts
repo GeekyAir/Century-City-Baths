6f8f9548edffbe90e0cc85a943da636d24366a9c
Farha Ahmed <farhaa853@gmail.com>
2024-06-04 12:39:54 +0300

    walk-in-showers

---
+++ src/app/components/walk-in-showers/walk-in-showers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-walk-in-showers',
+  standalone: true,
+  imports: [],
+  templateUrl: './walk-in-showers.component.html',
+  styleUrl: './walk-in-showers.component.scss'
+})
+export class WalkInShowersComponent {
+
+}
