5af84541795dc4fd320de66501ba5e182c0fabd9
Farha Ahmed <farhaa853@gmail.com>
2024-06-04 14:22:30 +0300

    bathtub page

--- src/app/components/bathroom/bathtub-replacement/bathtub-replacement.component.ts
+++ src/app/components/bathroom/bathtub-replacement/bathtub-replacement.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { FormComponent } from '../../../shared/form/form.component';
 
 @Component({
   selector: 'app-bathtub-replacement',
   standalone: true,
-  imports: [],
   templateUrl: './bathtub-replacement.component.html',
-  styleUrl: './bathtub-replacement.component.scss'
+  styleUrl: './bathtub-replacement.component.scss',
+  imports: [CardModule, FormComponent],
 })
-export class BathtubReplacementComponent {
-
-}
+export class BathtubReplacementComponent {}
