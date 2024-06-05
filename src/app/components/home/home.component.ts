ffc94bdc013dcf614281c58a2c3187336779bbeb
Farha Ahmed <farhaa853@gmail.com>
2024-06-05 13:12:44 +0300

    update home and about us

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormComponent } from '../../shared/form/form.component';
 import { CardModule } from 'primeng/card';
+import { ServicesComponent } from '../../shared/services/services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormComponent, CardModule],
+  imports: [FormComponent, CardModule, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent { }
+export class HomeComponent {}
