b6da1e07ded9cb5212e17e73b870be8a035bc538
rania-ibrhem30 <raniamansour967@gmail.com>
2024-06-05 13:15:36 +0300

    windows-compoents

--- src/app/components/windows/bay-and-bow/bay-and-bow.component.ts
+++ src/app/components/windows/bay-and-bow/bay-and-bow.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SidePanelComponent } from '../../../shared/side-panel/side-panel.component';
+import { ServicesComponent } from '../../../shared/services/services.component';
 
 @Component({
   selector: 'app-bay-and-bow',
   standalone: true,
-  imports: [],
+  imports: [SidePanelComponent, ServicesComponent],
   templateUrl: './bay-and-bow.component.html',
   styleUrl: './bay-and-bow.component.scss'
 })
--- src/app/components/windows/double-hung/double-hung.component.ts
+++ src/app/components/windows/double-hung/double-hung.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SidePanelComponent } from '../../../shared/side-panel/side-panel.component';
+import { ServicesComponent } from '../../../shared/services/services.component';
 
 @Component({
   selector: 'app-double-hung',
   standalone: true,
-  imports: [],
+  imports: [SidePanelComponent, ServicesComponent],
   templateUrl: './double-hung.component.html',
   styleUrl: './double-hung.component.scss'
 })
