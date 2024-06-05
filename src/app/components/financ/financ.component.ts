eabb602f087cb0c792f2e432ae75ba9b8e28ce54
rania-ibrhem30 <raniamansour967@gmail.com>
2024-06-05 15:27:07 +0300

    Financing

---
+++ src/app/components/financ/financ.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financ',
+  standalone: true,
+  imports: [],
+  templateUrl: './financ.component.html',
+  styleUrl: './financ.component.scss'
+})
+export class FinancComponent {
+
+}
