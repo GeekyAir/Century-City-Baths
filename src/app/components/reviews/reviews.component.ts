3d6f7072ef653c9b0d21dff772ab2835916a92c4
rania-ibrhem30 <raniamansour967@gmail.com>
2024-06-26 12:44:31 +0300

    error reviews

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -5,9 +5,11 @@ import { RouterLink } from '@angular/router';
   selector: 'app-reviews',
   standalone: true,
   imports: [RouterLink],
+  host: { ngSkipHydration: 'true' },
+
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss',
 })
 export class ReviewsComponent {
-  addReview() {}
+  addReview() { }
 }
